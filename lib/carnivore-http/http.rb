4dcce91a7e4a476e75930bc3753f79c080fd6d09
Chris Roberts <chrisroberts.code@gmail.com>
2013-10-18 10:01:53 -0700

    Add more flexibility to `transmit` method

--- lib/carnivore-http/http.rb
+++ lib/carnivore-http/http.rb
@@ -20,24 +20,43 @@ module Carnivore
         }.merge(args)
       end
 
-      def transmit(message, orignal_message_or_connection, args={})
-        if(original_message_or_connection.is_a?(Message))
-          con = original_message_or_connection[:connection]
-        else
-          con = original_message_or_connection[:connection]
+      # Transmit can be one of two things:
+      # 1. Response back to an open connection
+      # 2. Request to a remote source
+      def transmit(message, *extra)
+        options = extra.detect{|x| x.is_a?(Hash)} || {}
+        orig = extra.detect{|x| x.is_a?(Carnivore::Message)}
+        con = options[:connection]
+        if(orig && con.nil?)
+          con = orig[:connection]
         end
-        # TODO: add `args` options for marshaling: json/xml/etc
-        con.respond(args[:code] || :ok, message)
+        if(con) # response
+          payload = message.is_a?(String) ? message : MultiJson.dump(message)
+          # TODO: add `options` options for marshaling: json/xml/etc
+          debug "Transmit response type with payload: #{payload}"
+          con.respond(options[:code] || :ok, payload)
+        else # request
+          url = File.join("http://#{args[:bind]}:#{args[:port]}", options[:path].to_s)
+          method = (options[:method] || :post).to_sym
+          if(options[:headers])
+            base = HTTP.with_headers(options[:headers])
+          else
+            base = HTTP
+          end
+          payload = message.is_a?(String) ? message : MultiJson.dump(message)
+          debug "Transmit request type with payload: #{payload}"
+          base.send(method, url, :body => payload)
+        end
       end
 
       def process(*process_args)
         srv = Reel::Server.supervise(args[:bind], args[:port]) do |con|
           while(req = con.request)
             begin
-              msg = format(:request => req, :body => req.body, :connection => con)
+              msg = format(:request => req, :body => req.body.to_s, :connection => con)
               callbacks.each do |name|
                 c_name = callback_name(name)
-                debug "Dispatching message<#{msg[:message].object_id}> to callback<#{name} (#{c_name})>"
+                debug "Dispatching #{msg} to callback<#{name} (#{c_name})>"
                 Celluloid::Actor[c_name].async.call(msg)
               end
               con.respond(:ok, 'So long, and thanks for all the fish!') if args[:auto_respond]
