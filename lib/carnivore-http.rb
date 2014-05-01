254a5eba579a19eb5fed1be3465c933cb0c27dbf
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-01 07:23:48 -0700

    Ensure multi-json has been loaded

--- lib/carnivore-http.rb
+++ lib/carnivore-http.rb
@@ -1,5 +1,6 @@
 require 'carnivore-http/version'
 require 'carnivore'
+require 'multi_json'
 
 Carnivore::Source.provide(:http, 'carnivore-http/http')
 Carnivore::Source.provide(:http_endpoints, 'carnivore-http/http_endpoints')
