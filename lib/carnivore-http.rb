fd296deb4d2e3f3619426a770d305e2640a1aefc
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-13 17:26:43 -0700

    Add loaders

--- lib/carnivore-http.rb
+++ lib/carnivore-http.rb
@@ -2,5 +2,22 @@ require 'carnivore-http/version'
 require 'carnivore'
 require 'multi_json'
 
+module Carnivore
+  # HTTP namespace
+  module Http
+    autoload :PointBuilder, 'carnivore-http/point_builder'
+  end
+
+  module Source
+    autoload :Http, 'carnivore-http/http'
+    autoload :HttpEndpoints, 'carnivore-http/http_endpoints'
+  end
+
+  autoload :Utils, 'carnivore-http/utils'
+
+  # Compat for old naming
+  autoload :PointBuilder, 'carnivore-http/point_builder'
+end
+
 Carnivore::Source.provide(:http, 'carnivore-http/http')
 Carnivore::Source.provide(:http_endpoints, 'carnivore-http/http_endpoints')
