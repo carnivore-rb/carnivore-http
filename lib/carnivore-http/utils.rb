f5502802f67120f7787b1a93ee2d7ea44aee2dec
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-13 17:23:45 -0700

    Add some docs to util

--- lib/carnivore-http/utils.rb
+++ lib/carnivore-http/utils.rb
@@ -1,18 +1,27 @@
 module Carnivore
   module Http
+
+    # Helper utilities
     module Utils
+
+      # URL parameter helpers
       module Params
 
         class << self
 
+          # Load cgi library on demand
+          #
+          # @param klass [Class]
           def included(klass)
             require 'cgi'
           end
 
         end
 
-        # string:: HTTP query string
-        # Return Hash of parsed query string
+        # Generate hash of parsed query String
+        #
+        # @param string [String] HTTP query string
+        # @return [Hash]
         def parse_query_string(string)
           unless(string.to_s.empty?)
             args = CGI.parse(string)
@@ -22,8 +31,10 @@ module Carnivore
           end
         end
 
-        # args:: HTTP query string Hash
-        # Return formatted hash with inferred types
+        # Cast hash values when possible
+        #
+        # @param args [Hash]
+        # @return [Hash]
         def format_query_args(args)
           new_args = {}
           args.each do |k, v|
@@ -41,8 +52,10 @@ module Carnivore
           new_args
         end
 
-        # obj:: object
-        # Attempts to return true type
+        # Best attempt to convert to true type
+        #
+        # @param obj [Object] generally string value
+        # @return [Object] result of best attempt
         def format_query_type(obj)
           case obj
           when 'true'
