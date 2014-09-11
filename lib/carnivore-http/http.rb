09a0a51c8e5f325c1a0dafe915ac81b96fd94a7e
Chris Roberts <chrisroberts.code@gmail.com>
2014-09-11 15:40:55 -0700

    Enable delivery retry

--- lib/carnivore-http/http.rb
+++ lib/carnivore-http/http.rb
@@ -9,16 +9,55 @@ module Carnivore
     # Carnivore HTTP source
     class Http < Source
 
+      trap_exit :retry_delivery_failure
+
       include Carnivore::Http::Utils::Params
 
       # @return [Hash] source arguments
       attr_reader :args
+      # @return [Carnivore::Http::RetryDelivery]
+      attr_reader :retry_delivery
 
       # Setup the source
       #
       # @params args [Hash] setup arguments
       def setup(args={})
+        require 'fileutils'
         @args = default_args(args)
+        if(retry_directory)
+          @retry_delivery = Carnivore::Http::RetryDelivery.new(retry_directory)
+          self.link retry_delivery
+        end
+      end
+
+      # Handle failed retry deliveries
+      #
+      # @param actor [Object] terminated actor
+      # @param reason [Exception] reason for termination
+      # @return [NilClass]
+      def retry_delivery_failure(actor, reason)
+        if(actor == retry_delivery)
+          error "Failed RetryDelivery encountered: #{reason}. Rebuilding."
+          @retry_delivery = Carnivore::Http::RetryDelivery.new(retry_directory)
+        else
+          error "Unknown actor failure encountered: #{reason}"
+        end
+        nil
+      end
+
+      # @return [String, NilClass] directory storing failed messages
+      def retry_directory
+        if(args[:retry_directory])
+          FileUtils.mkdir_p(args[:retry_directory])
+        end
+      end
+
+      # @return [String, NilClass] cache directory for initial writes
+      def retry_write_directory
+        base = retry_directory
+        if(base)
+          FileUtils.mkdir_p(File.join(base, '.write'))
+        end
       end
 
       # Default configuration arguments. If hash is provided, it
@@ -30,7 +69,8 @@ module Carnivore
         Smash.new(
           :bind => '0.0.0.0',
           :port => '3000',
-          :auto_respond => true
+          :auto_respond => true,
+          :retry_directory => '/tmp/.carnivore-resend'
         ).merge(args)
       end
 
@@ -55,19 +95,83 @@ module Carnivore
         if(con) # response
           payload = message.is_a?(String) ? message : MultiJson.dump(message)
           # TODO: add `options` options for marshaling: json/xml/etc
-          debug "Transmit response type with payload: #{payload}"
-          con.respond(options[:code] || :ok, payload)
+          code = options.fetch(:code, :ok)
+          info "Transmit response type for #{message} with code: #{code}"
+          con.respond(code, payload)
         else # request
-          url = File.join("http://#{args[:bind]}:#{args[:port]}", options[:path].to_s)
-          method = (options[:method] || :post).to_sym
-          if(options[:headers])
-            base = HTTP.with_headers(options[:headers])
+          if(args[:endpoint])
+            url = args[:endpoint]
           else
-            base = HTTP
+            url = "http#{'s' if args[:ssl]}://#{args[:bind]}"
+            if(args[:port])
+              url << ":#{args[:port]}"
+            end
+            url = URI.join(url, args.fetch(:path, '/')).to_s
           end
+          if(options[:path])
+            url = URI.join(url, options[:path].to_s)
+          end
+          method = options.fetch(:method,
+            args.fetch(:method, :post)
+          ).to_s.downcase.to_sym
           payload = message.is_a?(String) ? message : MultiJson.dump(message)
-          debug "Transmit request type with payload: #{payload}"
-          base.send(method, url, :body => payload)
+          info "Transmit request type for message #{message}"
+          perform_transmission(message, payload, method, url, options.fetch(:headers, {}))
+        end
+      end
+
+      # Transmit message to HTTP endpoint
+      #
+      # @param message [Carnivore::Message]
+      # @param payload [String] serialized payload
+      # @param method [Symbol] HTTP method (:get, :post, etc)
+      # @param url [String] endpoint URL
+      # @param headers [Hash] request headers
+      # @return [HTTP::Response]
+      def perform_transmission(message, payload, method, url, headers={})
+        begin
+          base = headers.empty? ? HTTP : HTTP.with_headers(headers)
+          uri = URI.parse(url)
+          if(uri.userinfo)
+            base = base.basic_auth(:user => uri.user, :pass => uri.password)
+          end
+          result = base.send(method, url, :body => payload)
+          if(result.code < 200 || result.code > 299)
+            error "Invalid response code received for #{message}: #{result.code} - #{result.reason}"
+            write_for_retry(message.object_id, payload, method, url, headers)
+          end
+        rescue => e
+          error "Transmission failure (#{message}) - #{e.class}: #{e}"
+          debug "#{e.class}: #{e}\n#{e.backtrace.join("\n")}"
+          write_for_retry(message.object_id, payload, method, url, headers)
+        end
+      end
+
+      # Persist message if enabled for send retry
+      #
+      # @param message_id [String] ID of originating message
+      # @param payload [String] serialized payload
+      # @param method [Symbol] HTTP method (:get, :post, etc)
+      # @param url [String] endpoint URL
+      # @param headers [Hash] request headers
+      # @return [TrueClass, FalseClass] message persisted
+      def write_for_retry(message_id, payload, method, url, headers)
+        data = {
+          :message_id => message_id,
+          :payload => payload,
+          :method => method,
+          :url => url,
+          :headers => headers
+        }
+        if(retry_directory)
+          stage_path = File.join(rewrite_retry_directory, "#{Celluloid.uuid}.json")
+          final_path = File.join(retry_directory, File.dirname(stage_path))
+          File.open(stage_path, 'w+') do |file|
+            file.write MultiJson.dump(data)
+          end
+          FileUtils.move(stage_path, final_path)
+          info "Failed message (ID: #{message_id}) persisted for resend"
+          true
         end
       end
 
---
+++ lib/carnivore-http/retry_delivery.rb
@@ -0,0 +1,87 @@
+require 'carnivore-http'
+
+module Carnivore
+  module Http
+
+    class RetryDelivery
+
+      include Celluloid
+
+      # @return [String] message directory
+      attr_reader :message_directory
+
+      # Create new instance
+      #
+      # @param directory [String] path to messages
+      # @return [self]
+      def initialize(directory)
+        @message_directory = directory
+        every(60){ attempt_redelivery }
+      end
+
+      # Attempt to deliver messages found in message directory
+      #
+      # @return [TrueClass, FalseClass] attempt was made
+      # @note will not attempt if attempt is currently in progress
+      def attempt_redelivery
+        attempt = false
+        begin
+          unless(@delivering)
+            @delivering = true
+            attempt = true
+            Dir.glob(File.join(message_directory, '*.json')).each do |file|
+              debug "Redelivery processing: #{file}"
+              begin
+                args = MultiJson.load(File.read(file)).to_smash
+                debug "Restored from file #{file}: #{args.inspect}"
+                if(redeliver(args[:message_id], args[:payload], args[:method], args[:url], args[:headers]))
+                  FileUtils.rm(file)
+                end
+              rescue => e
+                error "Failed to process file (#{file}): #{e.class}: #{e}"
+                debug "#{e.class}: #{e}\n#{e.backtrace.join("\n")}"
+              end
+            end
+          end
+        rescue => e
+          error "Unexpected error encountered during message redelivery! #{e.class}: #{e}"
+          debug "#{e.class}: #{e}\n#{e.backtrace.join("\n")}"
+        ensure
+          @delivering = false
+        end
+        attempt
+      end
+
+      # Attempt redelivery of message
+      #
+      # @param message [Carnivore::Message]
+      # @param payload [String] serialized payload
+      # @param method [Symbol] HTTP method (:get, :post, etc)
+      # @param url [String] endpoint URL
+      # @param headers [Hash] request headers
+      # @return [TrueClass, FalseClass] redelivery was successful
+      def redeliver(message_id, payload, method, url, headers)
+        begin
+          base = headers.empty? ? HTTP : HTTP.with_headers(headers)
+          uri = URI.parse(url)
+          if(uri.userinfo)
+            base = base.basic_auth(:user => uri.user, :pass => uri.password)
+          end
+          result = base.send(method, url, :body => payload)
+          if(result.code < 200 || result.code > 299)
+            error "Invalid response code received for #{message_id}: #{result.code} - #{result.reason}"
+            false
+          else
+            info "Successful delivery of message on retry! Message ID: #{message_id}"
+            true
+          end
+        rescue => e
+          error "Transmission redelivery failure (Message ID: #{message}) - #{e.class}: #{e}"
+          debug "#{e.class}: #{e}\n#{e.backtrace.join("\n")}"
+          false
+        end
+      end
+
+    end
+  end
+end
