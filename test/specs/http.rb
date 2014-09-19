83305b450a304e4eeb1b648037af9692a3958c82
Chris Roberts <chrisroberts.code@gmail.com>
2014-09-19 05:50:10 -0700

    Init source up front for spec

--- test/specs/http.rb
+++ test/specs/http.rb
@@ -4,62 +4,65 @@ require 'carnivore-http'
 
 describe 'Carnivore::Source::Http' do
 
-  describe 'Building an HTTP based source' do
-
-    it 'returns the source' do
-      Carnivore::Source.build(
-        :type => :http,
-        :args => {
-          :name => :http_source,
-          :bind => '127.0.0.1',
-          :port => '8705'
-        }
-      )
-      t = Thread.new{ Carnivore.start! }
-      source_wait
-      Carnivore::Supervisor.supervisor[:http_source].wont_be_nil
-      t.terminate
+  before do
+    MessageStore.init
+    Carnivore::Source.build(
+      :type => :http,
+      :args => {
+        :name => :http_source,
+        :bind => '127.0.0.1',
+        :port => '8705'
+      }
+    ).add_callback(:store) do |message|
+      MessageStore.messages.push(message[:message][:body])
+      message.confirm!
     end
+    @runner = Thread.new{ Carnivore.start! }
+    source_wait
+  end
 
+  after do
+    @runner.terminate
   end
 
   describe 'HTTP source based communication' do
+
     before do
-      MessageStore.init
-      Carnivore::Source.build(
-        :type => :http,
-        :args => {
-          :name => :http_source,
-          :bind => '127.0.0.1',
-          :port => '8705'
-        }
-      ).add_callback(:store) do |message|
-        MessageStore.messages.push(message[:message][:body])
-      end
-      @runner = Thread.new{ Carnivore.start! }
-      source_wait
+      MessageStore.messages.clear
     end
 
-    after do
-      @runner.terminate
+    describe 'Building an HTTP based source' do
+
+      it 'returns the source' do
+        Carnivore::Supervisor.supervisor[:http_source].wont_be_nil
+      end
+
     end
 
     describe 'message transmissions' do
+
       it 'should accept message transmits' do
         Carnivore::Supervisor.supervisor[:http_source].transmit('test message')
       end
 
       it 'should receive messages' do
         Carnivore::Supervisor.supervisor[:http_source].transmit('test message 2')
-        source_wait
+        source_wait(2) do
+          !MessageStore.messages.empty?
+        end
+        MessageStore.messages.wont_be_empty
         MessageStore.messages.pop.must_equal 'test message 2'
       end
 
       it 'should accept http requests' do
         HTTP.get('http://127.0.0.1:8705/')
-        source_wait
+        source_wait(2) do
+          !MessageStore.messages.empty?
+        end
+        MessageStore.messages.wont_be_empty
         MessageStore.messages.pop.wont_be_nil
       end
+
     end
   end
 
