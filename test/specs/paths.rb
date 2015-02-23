79ef117a60e78a5b02e4593b0f2f3a1c0678f395
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-23 11:02:01 -0800

    Update path based specs to include globing, direct http transmissions, and custom responses

--- test/specs/paths.rb
+++ test/specs/paths.rb
@@ -16,11 +16,12 @@ describe 'Carnivore::Source::Http' do
           :path => '/fubar',
           :method => :post,
           :bind => '127.0.0.1',
-          :port => '8706'
+          :port => '8706',
+          :auto_respond => false
         }
       ).add_callback(:store) do |message|
         MessageStore.messages.push(message[:message][:body])
-        message.confirm!
+        message.confirm!(:response_body => 'custom response')
       end
       Carnivore::Source.build(
         :type => :http_paths,
@@ -33,7 +34,18 @@ describe 'Carnivore::Source::Http' do
         }
       ).add_callback(:store) do |message|
         MessageStore.messages.push(message[:message][:body])
-        message.confirm!
+      end
+      Carnivore::Source.build(
+        :type => :http_paths,
+        :args => {
+          :name => :glob_source,
+          :path => '/glob/v*/*',
+          :method => :get,
+          :bind => '127.0.0.1',
+          :port => '8706'
+        }
+      ).add_callback(:store) do |message|
+        MessageStore.messages.push(message[:message][:body])
       end
       @runner = Thread.new{ Carnivore.start! }
       source_wait
@@ -59,7 +71,7 @@ describe 'Carnivore::Source::Http' do
 
     end
 
-    describe 'message transmissions' do
+    describe 'source message transmissions' do
 
       it 'should accept message transmits' do
         Carnivore::Supervisor.supervisor[:fubar_source].transmit('test message')
@@ -79,6 +91,31 @@ describe 'Carnivore::Source::Http' do
         end
         MessageStore.messages.wont_be_empty
         MessageStore.messages.pop.must_equal 'test message to ohai'
+      end
+
+    end
+
+    describe 'HTTP message transmissions' do
+
+      it 'should receive messages and provide custom response' do
+        response = HTTP.post('http://127.0.0.1:8706/fubar', :body => 'test')
+        response.body.to_s.must_equal 'custom response'
+        source_wait{ !MessageStore.messages.empty? }
+        MessageStore.messages.pop.must_equal 'test'
+      end
+
+      it 'should receive messages and provide default response' do
+        response = HTTP.get('http://127.0.0.1:8706/ohai')
+        source_wait{ !MessageStore.messages.empty? }
+        response.body.to_s.must_equal 'So long and thanks for all the fish!'
+        MessageStore.messages.pop.must_be :empty?
+      end
+
+      it 'should receive messages via glob matching' do
+        response = HTTP.get('http://127.0.0.1:8706/glob/v2/things')
+        source_wait{ !MessageStore.messages.empty? }
+        response.body.to_s.must_equal 'So long and thanks for all the fish!'
+        MessageStore.messages.pop.must_be :empty?
       end
 
     end
